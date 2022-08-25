1664405e5224ee7e2c1352aafd86bdd17cce7934
Zappar <hello@zappar.com>
2022-08-25 12:26:18 +0100

    Minimal project for Zappar BabylonJS es6-image-tracking

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { Engine, HemisphericLight, Mesh, Scene, Vector3 } from '@babylonjs/core';
+import * as BABYLON from '@babylonjs/core';
 import * as ZapparBabylon from '@zappar/zappar-babylonjs-es6';
-
 import "./style.css";
+
 
 
 // Setup BabylonJS in the usual way
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 
-const engine = new Engine(canvas, true, {
+const engine = new BABYLON.Engine(canvas, true, {
     preserveDrawingBuffer: true,
     stencil: true
 });
 
-export const scene = new Scene(engine);
-const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
+export const scene = new BABYLON.Scene(engine);
+const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
 
 // Setup a Zappar camera instead of one of Babylon's cameras
 export const camera = new ZapparBabylon.Camera('camera', scene);
@@ -26,11 +26,13 @@ ZapparBabylon.permissionRequestUI().then((granted) => {
 });
 
 
-const imageTracker = new ZapparBabylon.ImageTrackerLoader().load(require("file-loader!./example-tracking-image.zpt").default);
+const url = new URL("./example-tracking-image.zpt", import.meta.url).href;
+const imageTracker = new ZapparBabylon.ImageTrackerLoader().load(url);
+
 const trackerTransformNode = new ZapparBabylon.ImageAnchorTransformNode('tracker', camera, imageTracker, scene);
 
 // Add some content to the image tracker
-const box = Mesh.CreateBox('box', 1, scene, false, Mesh.DOUBLESIDE)
+const box = BABYLON.Mesh.CreateBox('box', 1, scene, false, BABYLON.Mesh.DOUBLESIDE)
 box.parent = trackerTransformNode;
 box.visibility = 0;
 
